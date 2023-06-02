e6a06e570c2ec51012eba0ada752cf07e82b84c8
SuperStevePrice <SuperStevePrice@gmail.com>
2023-06-01 17:42:19 -0700

    Merge branch 'main' of github.com:SuperStevePrice/ShellSetup

--- bin/xterm.pl
+++ bin/xterm.pl
@@ -21,29 +21,31 @@ use strict;
 use warnings;
 use feature 'say';
 
-my $xterm = qx[which xterm];
+my $xterm_path = "/opt/X11/bin/xterm";
 
 # Define and initialize parameters object:
 my $params = {
     bg   => "CadetBlue",
     fg   => "Black",
-    fn   => "9x15bold",
+    fn   => "Courier New Bold",
+    fn_size => "16",
     cols => "80",
     rows => "45",
     sl   => "200",
     log  => 'N',
-    cmd  => 'N',
+    cmd_log  => 'N',
 };
 
 # Subroutine to prompt user and return input:
 sub prompt_user {
-	my ( $prompt, $default ) = @_;
-	my $default_text = defined $default ? "[$default]" : "";
-	print sprintf( "%-30s %s: ", $prompt, $default_text );
+    my ($question, $default) = @_;
+	my $default_prompt = defined $default ? " [$default]" : "";
+	my $padding = " " x (40 - length($question) - length($default_prompt));
+	printf("%s%s%s: ", $question, $padding, $default_prompt);
 	my $input = <STDIN>;
 	chomp $input;
-	return $input || $default;
-}
+	return $input ne "" ? $input : $default;
+}    # sub prompt_user
 
 # Subroutine to launch xterm
 sub Xterm {
@@ -53,30 +55,34 @@ sub Xterm {
     my $title = "steve\@ " . localtime();
 
     # Prompt for the shell
-    #my $shell = prompt_user( "Enter the shell (/bin/ksh)", "/bin/ksh" );
+    #my $shell = prompt_user( "Enter the shell (e.g., /bin/ksh)", "/bin/ksh" );
 
     # Determine the font parameter
-    my $font_param = $params->{fn} =~ /\s/ ? "-fa"              : "-fn";
-    my $font_value = $font_param eq "-fn" ? "\"$params->{fn}\"" : $params->{fn};
+    #kmy $font_param = $params->{fn} =~ /\s/ ? "-fa"              : "-fa";
+	my $font_param = " -fa ";
+    my $font_value = $font_param eq "-fa" ? "\"$params->{fn}\"" : $params->{fn};
 
     # Format the xterm command
-	my $cmd = "$xterm -sb -sl $params->{sl} $font_param $font_value ";
+    my $cmd = "$xterm_path -sb -sl $params->{sl} $font_param \"$font_value\" ";
+    $cmd .= "-fs $params->{fn_size} ";
     $cmd .= "-geometry $params->{cols}x$params->{rows} -fg \"$params->{fg}\" ";
     $cmd .= "-bg \"$params->{bg}\" -title \"$title\"";
     $cmd .= " -l" if lc( $params->{log} ) eq 'y';
+
+    #$cmd .= " -e '$shell'";
 
 	# Execute the xterm command in a separate process
 	my $pid = fork();
 
 	if ($pid == 0) {
 		# Child process
-		system(" " . $cmd);
+		system($cmd);
 		exit(0);
 	} elsif ($pid > 0) {
 		# Parent process
 		print "xterm launched in the background.\n";
 		write_command_log(${cmd})
-			if (lc($params->{cmd}) eq 'y')
+			if (lc($params->{cmd_log}) eq 'y')
 	} else {
 		# Forking failed
 		die "Failed to fork a child process: $!";
@@ -90,15 +96,19 @@ sub get_user_inputs() {
     print "\n";
     $params->{bg} = prompt_user( "Pick a background color", $params->{bg} );
     $params->{fg} = prompt_user( "Pick a foreground color", $params->{fg} );
-    $params->{fn} = prompt_user( "Enter the font", $params->{fn} );
+    $params->{fn} = prompt_user( "Enter the font)",         $params->{fn} );
+    $params->{fn_size} = 
+        prompt_user( "Enter the font size)", $params->{fn_size} );    
     $params->{cols} =
-      prompt_user( "Enter the number of columns", $params->{cols} );
+      prompt_user( "Enter the number of columns)", $params->{cols} );
     $params->{rows} =
-      prompt_user( "Enter the number of rows", $params->{rows} );
+      prompt_user( "Enter the number of rows)", $params->{rows} );
     $params->{sl} =
-      prompt_user( "Enter the memory buffer size", $params->{sl} );
-    $params->{log} = prompt_user( "Enable keystroke logging? (Y/N)", $params->{log} );
-    $params->{cmd} = prompt_user( "Enable command logging? (Y/N)", $params->{cmd} );
+      prompt_user( "Enter the scrollback lines size)", $params->{sl} );
+    $params->{log} = 
+        prompt_user( "Enable keystroke logging? (Y/N)", $params->{log} );
+    $params->{cmd_log} = 
+        prompt_user( "Enable command logging? (Y/N)", $params->{cmd_log} );
     print "\n";
 }    # sub get_user_inputs()
 
@@ -110,8 +120,7 @@ sub write_command_log {
 	my $command_log = "/Users/steve/Documents/xterm.log";
 	say "See debug file: $command_log\n";
 	open(my $fh, '>>', $command_log) or die "Failed to open $command_log: $!";
-	$cmd.="\n";
-	say $fh "\n $cmd";
+	say $fh $cmd . "\n";
 	close($fh);
 	return $command_log;
 }
@@ -119,5 +128,3 @@ sub write_command_log {
 # Launch xterm
 get_user_inputs();
 Xterm($params);
-
-#-------------------------------------------------------------------------------
--- bin/xterm.py
+++ bin/xterm.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 
-import subprocess
 import os
 import socket
 import shutil
+import sys
 from datetime import datetime
+
+# Default values
+default_values = {
+    "background_color": "CadetBlue",
+    "foreground_color": "Black",
+    "font": "Courier New Bold",
+    "font_size": "16",
+    "columns": "80",
+    "rows": "45",
+    "scrollback_lines": "200",
+    "enable_keystroke_logging": False,
+    "enable_command_logging": False
+}
+
 
 def write_command_log_file(cmd):
     cmd_log_file = os.path.expanduser('~/Documents/xterm.log')
@@ -25,6 +39,14 @@ def write_command_log_file(cmd):
             file.write('\n')
 
 
+def prompt_user(question, default):
+    default_prompt = f" [{default}]" if default else ""
+    padding = " " * (40 - len(question) - len(default_prompt))
+    #print("DEBUG: ", len(padding), "\n")
+    print(f"{question}{padding}{default_prompt}: ", end="")
+    user_input = input()
+    return user_input if user_input else default
+
 
 def Xterm(params):
     success = 0
@@ -42,28 +64,43 @@ def Xterm(params):
             sys.exit(1)
 
     # Gather user preferences
-    background_color = input("Pick a background color [CadetBlue]: ") or "CadetBlue"
-    foreground_color = input("Pick a foreground color [Black]: ") or "Black"
-    font = input("Enter the font [9x15bold]: ") or "9x15bold"
-    columns = input("Enter the number of columns [80]: ") or "80"
-    rows = input("Enter the number of rows [45]: ") or "45"
-    buffer_size = input("Enter the memory buffer size [200]: ") or "200"
-    enable_keystroke_logging = input("Enable keystroke logging? (Y/N) [N]: ") == "Y"
-    enable_command_logging = input("Enable command logging? (Y/N) [N]: ") == "Y"
+    background_color = prompt_user(
+        "Pick a background color", default_values["background_color"])
+    foreground_color = prompt_user(
+        "Pick a foreground color", default_values["foreground_color"])
+    font = prompt_user("Enter the font", default_values["font"])
+    font_size = prompt_user("Enter the font size", default_values["font_size"])
+    columns = prompt_user("Enter the number of columns", default_values["columns"])
+    rows = prompt_user("Enter the number of rows", default_values["rows"])
+    scrollback_lines = prompt_user("Enter the scrollback lines size",
+                              default_values["scrollback_lines"])
+
+    enable_keystroke_logging = prompt_user(
+        "Enable keystroke logging? (Y/N)", "Y" if default_values["enable_keystroke_logging"] else "N") == "Y"
+    enable_command_logging = prompt_user(
+        "Enable command logging? (Y/N)", "Y" if default_values["enable_command_logging"] else "N") == "Y"
+
+    if enable_keystroke_logging:
+        log = " -l "
+    else:
+        log = ""
 
     # Build the xterm command
     cmd = [
         xterm_executable,
         "-sb",
         "-sl",
-        buffer_size,
-        "-fn",
-        font,
+        scrollback_lines,
+        "-fa",
+        f"'{font}'",
+        "-fs",
+        font_size,
         "-geometry",
         f"{columns}x{rows}",
         "-fg",
         foreground_color,
         "-bg",
+        log,
         background_color,
         "-title",
         f"'{os.environ['USER']}@{socket.gethostname()} {datetime.now()}'"
@@ -78,11 +115,12 @@ def Xterm(params):
         exit(0)
     elif pid > 0:
         # Parent process
-        print("xterm launched in the background.")
+        print("\nxterm launched in the background.\n")
         write_command_log_file(cmd)
         if enable_command_logging:
             try:
                 write_command_log_file(cmd)
+                print("The xterm command has been written to the xterm.log.")
             except Exception as e:
                 print(f"Error writing to log file: {str(e)}")
     else:
@@ -91,4 +129,3 @@ def Xterm(params):
 
 # Call the Xterm function
 Xterm({})
-
